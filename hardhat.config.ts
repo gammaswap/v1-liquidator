c58fac0aa914c95742b94d90956e07f56c9bbe88
danielalcarraz <dam417@nyu.edu>
2023-04-22 01:03:35 -0400

    set up hardhat config file with contract sizer, solidity version 0.8.17, accounts task, dotenv, and goerli network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,54 @@
-import { HardhatUserConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig , task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
+import "hardhat-contract-sizer"; // "npx hardhat size-contracts" or "yarn run hardhat size-contracts"
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts:
+          process.env.GOERLI_PRIVATE_KEY !== undefined
+              ? [process.env.GOERLI_PRIVATE_KEY]
+              : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
