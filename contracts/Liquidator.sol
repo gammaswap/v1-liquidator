0bd256790a5fd33e422fb5d66fc3e99ee7450f7c
danielalcarraz <dam417@nyu.edu>
2023-08-29 15:02:15 -0400

    use solidity compiler version 0.8.21

--- contracts/Liquidator.sol
+++ contracts/Liquidator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-v3
-pragma solidity 0.8.17;
+pragma solidity 0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@gammaswap/v1-core/contracts/interfaces/IPoolViewer.sol";
